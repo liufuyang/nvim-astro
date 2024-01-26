3b22b1a403598a8dbe0dce7c585783d848fa05ac
Fuyang Liu <liufuyang@users.noreply.github.com>
2024-01-27 00:52:27 +0100

    minor ui tweak and double space for find files

--- lua/user/init.lua
+++ lua/user/init.lua
@@ -19,6 +19,27 @@ return {
         },
       },
     },
+  },
 
+  -- https://docs.astronvim.com/recipes/status/
+  -- Update the color of insert mode tag
+  heirline = {
+    colors = {
+      -- To match up with gruvbox
+      -- https://github.com/ellisonleao/gruvbox.nvim/blob/4176b0b720db0c90ab4030e5c1b4893faf41fd51/lua/gruvbox.lua#L90
+      insert = "#fe8019",
+      normal = "#83a598",
+      visual = "#d3869b",
+    },
+  },
+
+  -- https://docs.astronvim.com/recipes/mappings/
+  mappings = {
+    n = {
+     ["<leader><space>"] = {
+        function() require("telescope.builtin").find_files() end,
+        desc = "Find files"
+      }
+    },
   },
 }
